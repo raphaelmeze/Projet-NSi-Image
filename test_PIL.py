354a22561ffc2ce1e3356a18014e7e25dfcfa942
raphaelmeze <162619688+raphaelmeze@users.noreply.github.com>
2024-03-26 13:33:04 +0100

    Update test_PIL.py

--- test_PIL.py
+++ test_PIL.py
@@ -75,3 +75,21 @@ def noir_et_blanc(image,seuil):
             else:
                 image.putpixel((x,y),(0,0,0))
     image.show()
+
+def negatif(image):
+    for x in range (image.width): 
+        for y in range (image.height): 
+            r, v, b=image.getpixel((x,y))
+            image.putpixel((x,y),(255-r,255-v,255-b)) 
+    image.show() 
+def premier_plan (image):
+    largeur, longueur = image.width, image.height
+    for x in range (image.width): 
+        for y in range (image.height): 
+            if not((x>largeur//3 and x<largeur*2//3) and (y>longueur//3 and y<longueur*2//3)):
+                rouge=image.getpixel((x,y))[0] 
+                bleu=image.getpixel((x,y))[1] 
+                vert=image.getpixel((x,y))[2] 
+                m=(rouge+bleu+vert)//3 
+                image.putpixel((x,y),(m,m,m))
+    image.show()
