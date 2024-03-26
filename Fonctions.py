a0b623111b584b712066e222e2e1893387e2fa6c
raphaelmeze <162619688+raphaelmeze@users.noreply.github.com>
2024-03-26 14:42:35 +0100

    Update Fonctions.py

--- Fonctions.py
+++ Fonctions.py
@@ -11,3 +11,36 @@ def noir_et_blanc(image,seuil):
             else:
                 image_nb.putpixel((x,y),(0,0,0))
     image_nb.show()
+def niveaux_de_gris(image): 
+    img2=image.copy()
+    for x in range (img2.width): 
+        for y in range (img2.height): 
+            rouge=img2.getpixel((x,y))[0] 
+            bleu=img2.getpixel((x,y))[1] 
+            vert=img2.getpixel((x,y))[2] 
+            m=(rouge+bleu+vert)//3 
+            img2.putpixel((x,y),(m,m,m)) 
+    return img2 
+
+def contours(image,seuil):
+    img2=image.copy()
+    niveaux_de_gris(img2)
+    for x in range (img2.width-1): 
+        for y in range (img2.height): 
+            if x==0: 
+                couleur1=img2.getpixel((x,y))[0] 
+                couleur2=img2.getpixel((x+1,y))[0] 
+                difference=abs(couleur1-couleur2)
+                if difference>seuil: 
+                    img2.putpixel((x,y),(255,255,255)) 
+                else: 
+                    img2.putpixel((x,y),(0,0,0))
+            else: 
+                couleur1=image.getpixel((x,y))[0] 
+                couleur2=image.getpixel((x+1,y))[0] 
+                difference=abs(couleur1-couleur2)
+                if difference>seuil: 
+                    img2.putpixel((x,y),(255,255,255)) 
+                else: 
+                    img2.putpixel((x,y),(0,0,0)) 
+    return img2
